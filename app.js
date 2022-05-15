128de6b3a7392990070d567907e2f83a6ba5aed7
AbdulRehman <likeam99@gmail.com>
2022-05-16 04:24:49 +0500

    Building the review section

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-conatiner');
-let loginForm = document.querySelector('.login-form-container');
-let formBtn = document.querySelector('.login-btn');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let videoBtn = document.querySelectorAll('vid-btn');
+// let searchBtn = document.querySelector('#search-btn');
+// let searchBar = document.querySelector('.search-bar-conatiner');
+// let loginForm = document.querySelector('.login-form-container');
+// let formBtn = document.querySelector('.login-btn');
+// let formClose = document.querySelector('#form-close');
+// let menu = document.querySelector('#menu-bar');
+// let navbar = document.querySelector('.navbar');
+// let videoBtn = document.querySelectorAll('vid-btn');
 
-window.onscroll = () =>{
-    searchBtn.classList.remove('fa-times');
-    searchBar.classList.remove('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
-}
+// window.onscroll = () =>{
+//     searchBtn.classList.remove('fa-times');
+//     searchBar.classList.remove('active');
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+//     loginForm.classList.remove('active');
+// }
 
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-});
+// menu.addEventListener('click', () =>{
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// });
 
-searchBtn.addEventListener('click', () =>{
-    searchBtn.classList.toggle('fa-times');
-    searchBar.classList.toggle('active');
-});
+// searchBtn.addEventListener('click', () =>{
+//     searchBtn.classList.toggle('fa-times');
+//     searchBar.classList.toggle('active');
+// });
 
-formBtn.addEventListener('click', () =>{
-    loginForm.classList.add('active');
-});
+// formBtn.addEventListener('click', () =>{
+//     loginForm.classList.add('active');
+// });
 
-formClose.addEventListener('click', () =>{
-    loginForm.classList.remove('active');
-});
+// formClose.addEventListener('click', () =>{
+//     loginForm.classList.remove('active');
+// });
 
-videoBtn.forEach(btn => {
-    btn.addEventListener('click', () =>{
-            document.querySelector('.control .active').classList.remove('active');
-            btn.classList.add('active');
-            let src = btn.getAttribute('data-src');
-            document.querySelector('#video-slider').src = src;
-    });
+// videoBtn.forEach(btn => {
+//     btn.addEventListener('click', () =>{
+//             document.querySelector('.control .active').classList.remove('active');
+//             btn.classList.add('active');
+//             let src = btn.getAttribute('data-src');
+//             document.querySelector('#video-slider').src = src;
+//     });
+// });
+
+let swiper = new Swiper(".review-slider", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
